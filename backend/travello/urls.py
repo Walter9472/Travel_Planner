ba1eea4fce315051fb66236446378cc59fa39c68
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-09 22:52:42 +0200

    Add responsive templates for travel planner

--- backend/travello/urls.py
+++ backend/travello/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('destination/<int:dest_id>/', views.destination, name='destination'),
+    path('trip/<int:trip_id>/', views.trip_detail, name='trip_detail'),
+    path('activity/<int:activity_id>/', views.activity_detail, name='activity_detail'),
 
 ]
