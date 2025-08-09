44fc0562be1641c642d0533a1ddefeaf3892ed6f
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-09 12:17:30 +0200

    Add root index view for travello app

--- backend/travello/views.py
+++ backend/travello/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    """Return a simple greeting for the Travello app."""
+    return HttpResponse("Welcome to Travello!")
+
