3b8b9dfd4e2e7fc9ce08a64f5528ea6b16c73656
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-09 19:30:08 +0200

    Register Activity and Destination models in admin with search functionality

--- backend/Travel_Planner/urls.py
+++ backend/Travel_Planner/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
