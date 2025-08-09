461d45838da7d32449604eeb1ed089fbffdcd57b
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-09 19:00:38 +0200

    Register Activity and Destination models in admin with search functionality

--- backend/travello/admin.py
+++ backend/travello/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 
 from .models import Destination, Trip, Activity
 
+# Register your models here.
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
     list_display = ("title", "destination", "start_date", "end_date")
 
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ("name", "desc")
+    search_fields = ("name", "desc")
 
-admin.site.register(Destination)
-admin.site.register(Activity)
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ("name", "country", "desc","price")
+    search_fields = ("name", "country")
 
